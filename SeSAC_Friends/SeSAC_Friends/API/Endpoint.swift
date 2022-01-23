86cf8cc1727d5606a014d4d63b3895d048a9a4ba
luboolu <kjjy98@naver.com>
2022-01-24 00:56:56 +0900

    first commit

---
+++ SeSAC_Friends/SeSAC_Friends/API/Endpoint.swift
@@ -0,0 +1,29 @@
+//
+//  Endpoint.swift
+//  SeSAC_Friends
+//
+//  Created by 김진영 on 2022/01/24.
+//
+
+import Foundation
+
+extension URL {
+    static let baseURL = "http://test.monocoding.com:35484"
+    
+    static func makeEndPoint(_ endpoint: String) -> URL {
+        URL(string: baseURL + endpoint)!
+    }
+    
+    static var user: URL {
+        return makeEndPoint("/user")
+    }
+    
+    static var userWithdraw: URL {
+        return URL(string: "\(URL.user)/withdraw")!
+    }
+    
+    static var userUpdateFCM: URL {
+        return URL(string: "\(URL.user)/update_fcm_token")!
+    }
+    
+}
