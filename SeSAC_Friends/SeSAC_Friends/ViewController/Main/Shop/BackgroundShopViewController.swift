375e1e8c45ef2a95e40cc808977425787c89c4a6
luboolu <kjjy98@naver.com>
2022-02-26 00:14:19 +0900

    SeSAC Shop UI

--- SeSAC_Friends/SeSAC_Friends/ViewController/Main/Shop/BackgroundShopViewController.swift
+++ SeSAC_Friends/SeSAC_Friends/ViewController/Main/Shop/BackgroundShopViewController.swift
@@ -13,6 +13,10 @@ import RxSwift
 final class BackgroundShopViewController: UIViewController {
     
     private let mainView = BackgroundShopView()
+    private let backgroundImage = ["sesac_background_1", "sesac_background_2", "sesac_background_3", "sesac_background_4", "sesac_background_5", "sesac_background_6", "sesac_background_7", "sesac_background_8"]
+    private let backgroundImageName = ["하늘 공원", "씨티 뷰", "밤의 산책로", "낮의 산책로", "연극 무대", "라틴 거실", "홈트방", "뮤지션 작업실"]
+    private let backgroundImageDescription = ["새싹들을 많이 마주치는 매력적인 하늘 공원입니다", "창밖으로 보이는 도시 야경이 아름다운 공간입니다", "어둡지만 무섭지 않은 조용한 산책로입니다", "즐겁고 가볍게 걸을 수 있는 산책로입니다", "연극의 주인공이 되어 연기를 펼칠 수 있는 무대입니다", "모노톤의 따스한 감성의 거실로 편하게 쉴 수 있는 공간입니다", "집에서 운동을 할 수 있도록 기구를 갖춘 방입니다", "여러가지 음악 작업을 할 수 있는 작업실입니다"]
+    private let backgroundImagePrice = ["1,200", "1,200", "1,200", "1,200", "2,500", "2,500", "2,500", "2,500"]
     
     override func loadView() {
         super.loadView()
@@ -30,11 +34,16 @@ final class BackgroundShopViewController: UIViewController {
 
 extension BackgroundShopViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8
+        return backgroundImage.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.SeSacBackgroundTableViewCell.id) as? SeSacBackgroundTableViewCell else { return UITableViewCell() }
+        
+        cell.backgroundImage.image = UIImage(named: backgroundImage[indexPath.row])
+        cell.backgroundName.text = backgroundImageName[indexPath.row]
+        cell.descriptionLabel.text = backgroundImageDescription[indexPath.row]
+        cell.priceLabel.text = backgroundImagePrice[indexPath.row]
         
         return cell
     }
--- SeSAC_Friends/SeSAC_Friends/ViewController/Main/Shop/CharacterShopViewController.swift
+++ SeSAC_Friends/SeSAC_Friends/ViewController/Main/Shop/CharacterShopViewController.swift
@@ -14,6 +14,11 @@ final class CharacterShopViewController: UIViewController {
     
     private let mainView = CharacterShopView()
     
+    private let characterImage = ["sesac_face_1", "sesac_face_2", "sesac_face_3", "sesac_face_4", "sesac_face_5"]
+    private let characterName = ["기본 새싹", "튼튼 새싹", "민트 새싹", "퍼플 새싹", "골드 새싹"]
+    private let characterDescription = ["새싹을 대표하는 기본 식물입니다. 다른 새싹들과 함꼐 하는 것을 좋아합니다.", "잎이 하나 더 자라나고 튼튼해진 새나라의 새싹으로 같이 있으면 즐거워집니다.", "호불호의 대명사! 상쾌한 향이 나서 허브가 대중화된 지역에서 많이 자랍니다.", "감정을 편안하게 쉬도록 하며 슬프고 우울한 감정을 진정시켜주는 멋진 새싹입니다.", "화려하고 멋있는 삶을 살며 돈과 인생을 플렉스 하는 자유분방한 새싹입니다."]
+    private let characterPrice = ["1,200", "1,200", "2,500", "2,500", "2,500"]
+    
     override func loadView() {
         super.loadView()
         self.view = mainView
@@ -32,11 +37,16 @@ final class CharacterShopViewController: UIViewController {
 extension CharacterShopViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return characterImage.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.SeSacCharacterCollectionViewCell.id, for: indexPath) as? SeSacCharacterCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.characterImage.image = UIImage(named: characterImage[indexPath.row])
+        cell.characterName.text = characterName[indexPath.row]
+        cell.descriptionLabel.text = characterDescription[indexPath.row]
+        cell.priceLabel.text = characterPrice[indexPath.row]
         
         return cell
     }
